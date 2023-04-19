ac9c0635a6339d47046498eaacb7262916828742
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-19 14:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 function letterCombinations(input_digit) {
-  //Complete the function
-	const digitMap={
-		"0":["0"],
-		"1":["1"],
-		"2":["a","b","c"],
-		"3":["d","e","f"],
-		"4":["g","h","i"],
-		"5":["j","k","l"],
-		"6":["m","n","o"],
-		"7":["p","q","r","s"],
-		"8":["t","u","v"],
-		"9":["w","x","y","z"],
-		"2":["a","b","c"]
-	};
-	if(input_digit.length===0)
-	{
-		return[];
-	}
-	if(input_digit.length===1)
-	{
-		return digitMap[input_digit];
-	}
-	const remainingComb=letterCombinations(input_digit.slice(1));
-	const currentLetter=digitMap[input_digit[0]];
-	const combination=[];
-	for(const letter of currentLetter){
-		for(const remainingcombination of ramainingComb){
-			combination.push(letter+remainingcombination);
-		}}
-	return combination.sort();
-	
+  const digitMap = {
+    "0": ["0"],
+    "1": ["1"],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
+  };
+  
+  if (input_digit.length === 0) {
+    return [];
+  }
+  
+  if (input_digit.length === 1) {
+    return digitMap[input_digit];
+  }
+  
+  const remainingComb = letterCombinations(input_digit.slice(1));
+  const currentLetter = digitMap[input_digit[0]];
+  const combination = [];
+  
+  for (const letter of currentLetter) {
+    for (const remainingCombination of remainingComb) {
+      combination.push(letter + remainingCombination);
+    }
+  }
+  
+  return combination.sort((a, b) => a.localeCompare(b));
 }
 
 module.exports = letterCombinations;
